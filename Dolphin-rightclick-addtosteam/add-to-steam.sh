70f7deeea0a80a2f236b69e8da338be192ef70a0
suchmememanyskill <suchmememanyskill@users.noreply.github.com>
2022-03-19 00:28:22 +0100

    Should probably also include the script in the main repo

---
+++ Dolphin-rightclick-addtosteam/add-to-steam.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo Hello! This script will add an 'add to steam' button to the right click menu of applications
+echo Making directories..
+mkdir -p ~/.local/share/kservices5/ServiceMenus
+mkdir -p ~/.bin
+
+echo Downloading files..
+curl https://raw.githubusercontent.com/suchmememanyskill/steam-deckt-addons/main/Dolphin-rightclick-addtosteam/SimpleSteamShortcutAdder -o ~/.bin/SimpleSteamShortcutAdder
+curl https://raw.githubusercontent.com/suchmememanyskill/steam-deckt-addons/main/Dolphin-rightclick-addtosteam/SimpleSteamShortcutAdder.desktop -o ~/.local/share/kservices5/ServiceMenus/SimpleSteamShortcutAdder.desktop
+
+echo Setting permissions..
+chmod a+x ~/.bin/SimpleSteamShortcutAdder
+
+read -p "Done! Press enter to exit"
