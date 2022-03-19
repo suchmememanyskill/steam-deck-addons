24202f88eacd41be896b32d565d61337bee57c62
suchmememanyskill <suchmememanyskill@users.noreply.github.com>
2022-03-19 23:26:13 +0100

    Add source to actual repository

---
+++ Dolphin-rightclick-addtosteam/SimpleSteamShortcutAdder/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace SimpleSteamShortcutAdder
+{
+    class Program
+    {
+        static void Fatal(string message)
+        {
+            Console.WriteLine($"[FATAL] {message}");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Fatal("Not enough or too many args!");
+                return;
+            }
+
+            SteamManager manager = new SteamManager();
+            if (!manager.InitialisePaths())
+            {
+                Fatal("Could not initialise paths. Does a shortcuts.vdf file exist? Try to add a non-steam game to steam");
+                return;
+            }
+
+            Console.WriteLine($"Accessing '{manager.VdfPath}'");
+
+            if (!manager.Read())
+            {
+                Fatal("Failed to read shortcuts.vdf");
+                return;
+            }
+
+            if (!manager.AddExe(args[0], Path.GetFileName(args[0])))
+            {
+                Fatal("Failed to add executable");
+                return;
+            }
+
+            if (!manager.Write())
+            {
+                Fatal("Failed to write to the shortcuts.vdf file");
+                return;
+            }
+        
+            Console.WriteLine("Finished! Exiting in 5 seconds. Restart steam to view the new shortcut");
+            Thread.Sleep(5000);
+        }
+    }
+}
---
+++ Dolphin-rightclick-addtosteam/SimpleSteamShortcutAdder/SteamManager.cs
@@ -0,0 +1,96 @@
+﻿
+using System;
+using System.IO;
+using System.Linq;
+using VDFMapper;
+using VDFMapper.ShortcutMap;
+using VDFMapper.VDF;
+
+namespace SimpleSteamShortcutAdder
+{
+    public class SteamManager
+    {
+        private string vdfPath = "";
+        private string gridPath = "";
+        private VDFMap? root;
+        public ShortcutRoot? ShortcutRoot { get; private set; }
+        public string VdfPath { get => vdfPath; private set => vdfPath = value; }
+
+
+        public bool InitialisePaths()
+        {
+            if (GetSteamShortcutPath.GetUserDataPath() == "")
+                return false;
+
+            if (GetSteamShortcutPath.GetCurrentlyLoggedInUser() <= 0)
+                return false;
+
+            vdfPath = GetSteamShortcutPath.GetShortcutsPath();
+            gridPath = GetSteamShortcutPath.GetGridPath();
+            return true;
+        }
+
+        public bool Read()
+        {
+            if (!File.Exists(vdfPath))
+                return false;
+
+            VDFStream stream = new VDFStream(vdfPath);
+            root = new VDFMap(stream);
+            ShortcutRoot = new ShortcutRoot(root);
+            stream.Close();
+            return true;
+        }
+
+        public bool Write()
+        {
+            File.WriteAllText(vdfPath, "");
+            BinaryWriter writer = new BinaryWriter(new FileStream(vdfPath, FileMode.OpenOrCreate));
+            root!.Write(writer, null);
+            writer.Close();
+            return true;
+        }
+
+        public bool AddExe(string path, string name)
+        {
+            if (!File.Exists(path))
+                return false;
+
+            name = name.Split(".").First();
+            ShortcutEntry entry = null;
+            
+            for (int i = 0; i < ShortcutRoot.GetSize(); i++)
+            {
+                ShortcutEntry existingEntry = ShortcutRoot!.GetEntry(i);
+                if (existingEntry.AppName == name)
+                {
+                    entry = existingEntry;
+                    break;
+                }
+            }
+
+            if (entry != null)
+            {
+                Console.WriteLine($"Updating entry {name}");
+            }
+            else
+            {
+                Console.WriteLine($"Adding {name}");
+                entry = ShortcutRoot!.AddEntry();
+            }
+            
+            entry.AppName = name;
+            entry.AppId = ShortcutEntry.GenerateSteamGridAppId(entry.AppName, "");
+            entry.StartDir = Path.GetDirectoryName(path);
+            entry.Exe = path;
+            
+            if (entry.Exe.Contains(" "))
+                entry.Exe = $"\"{entry.Exe}\"";
+
+            if (entry.StartDir.Contains(" "))
+                entry.StartDir = $"\"{entry.StartDir}\"";
+
+            return true;
+        }
+    }
+}
